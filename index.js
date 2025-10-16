71e771152053f069efc3e5b5ec7a827b4e7711cb
Brendon W <bwherry@midland.k12.ia.us>
2025-10-16 17:15:20 +0000

    gg

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let animalName = 'Cat';
 
+let height = 20;
+
+let isIndoorPet = True;
+
+
+
+
+document.getElementById("string").innerHTML = typeof animalName;
+document.getElementById("number").innerHTML = typeof height;
+document.getElementById("boolean").innerHTML = typeof isIndoorPet;
